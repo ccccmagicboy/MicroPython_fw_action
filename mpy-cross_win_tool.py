1e250a15b818bbe3967b8313afbc383b4e90e0d5
cccc <cuiwei_cv@163.com>
2020-02-03 13:55:25 +0800

    add a tool to fix the makefile file.

---
+++ mpy-cross_win_tool.py
@@ -0,0 +1,9 @@
+import fileinput
+import sys
+
+def replaceAll(file,searchExp,replaceExp):
+    for line in fileinput.input(file, inplace=1):
+        if searchExp in line:
+            line = line.replace(searchExp,replaceExp)
+        sys.stdout.write(line)
+        
