947990bf2e30afbdd8f2368bcfec34c2c217f29e
cccc <cuiwei_cv@163.com>
2020-02-05 13:27:23 +0800

    wakey wakey GitHub Actions

--- mpy-cross_win_tool.py
+++ mpy-cross_win_tool.py
@@ -1,5 +1,6 @@
 import fileinput
 import sys
+import os
 
 def replaceAll(file,searchExp,replaceExp):
     for line in fileinput.input(file, inplace=1):
@@ -7,12 +8,22 @@ def replaceAll(file,searchExp,replaceExp):
             line = line.replace(searchExp,replaceExp)
         sys.stdout.write(line)
         
-import os
-print(os.environ['PATH'])
-print(os.getcwd())
-mk_path = os.path.join(os.getcwd(), 'my_micropython', 'mpy-cross', 'Makefile')
-print(mk_path)
-replaceAll(mk_path, "PROG = mpy-cross","PROG = mpy-cross.exe")
-print("ok finished!")
+def main():
+    print(os.environ['PATH'])
+    print(os.getcwd())
+    mk_path = os.path.join(os.getcwd(), 'my_micropython', 'mpy-cross', 'Makefile')
+    print(mk_path)
+    replaceAll(mk_path, "PROG = mpy-cross","PROG = mpy-cross.exe")
+    print("ok finished!")
+    
+def get_ver():
+    command = 'cd my_micropython\mpy-cross && mpy-cross.exe --version'
+    print(command)
+    result = os.popen(command).read()
+    print(result)
+    return result
 
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
 
