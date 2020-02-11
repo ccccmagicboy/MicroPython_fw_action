1d8b9253f82c28a39ada326ea1703a35abb29ed1
cccc <cuiwei_cv@163.com>
2020-02-11 18:53:06 +0800

    wakey wakey GitHub Actions

--- c_modules/STM32/hello/hello.c
+++ c_modules/STM32/hello/hello.c
@@ -21,8 +21,6 @@ MP_DEFINE_CONST_FUN_OBJ_1(hello_world_obj, hello_world);
 
 // Copy the uncommented line into your map table
 STATIC const mp_rom_map_elem_t my_module_globals_table[] = {
-    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_builtins) },
+    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_hello) },
     { MP_ROM_QSTR(MP_QSTR_hello_world), MP_ROM_PTR(&hello_world_obj) },
-    // Example constant
-    { MP_ROM_QSTR(MP_QSTR_EXAMPLE_CONST), MP_ROM_INT(123) }
 };
