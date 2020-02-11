014a1b8d0a932648222c31515f3c3862431aaa4a
cccc <cuiwei_cv@163.com>
2020-02-11 18:36:32 +0800

    wakey wakey GitHub Actions

--- c_modules/STM32/hello/hello.c
+++ c_modules/STM32/hello/hello.c
@@ -1,4 +1,5 @@
 #include "py/runtime.h"
+#include <stdio.h>
 
 STATIC mp_obj_t hello_world(
         mp_obj_t echo_obj) {
@@ -6,21 +7,22 @@ STATIC mp_obj_t hello_world(
     bool result;
 
     // Your code here!
-    printf("echo this: %s", echo);
+    printf("the input is: %s\n", echo);
 
-    // Example exception
-    if (some_val == 0) {
-        mp_raise_ValueError("some_val can't be zero!");
-    }
+    result = 1;
+    
+    
+
+    
 
     return mp_obj_new_bool(result);
 }
 MP_DEFINE_CONST_FUN_OBJ_1(hello_world_obj, hello_world);
 
 // Copy the uncommented line into your map table
-// STATIC const mp_rom_map_elem_t my_module_globals_table[] = {
-    // { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_builtins) },
+STATIC const mp_rom_map_elem_t my_module_globals_table[] = {
+    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_builtins) },
     { MP_ROM_QSTR(MP_QSTR_hello_world), MP_ROM_PTR(&hello_world_obj) },
     // Example constant
-    { MP_ROM_QSTR(MP_QSTR_EXAMPLE_CONST), MP_ROM_INT(123) },
-// };
+    { MP_ROM_QSTR(MP_QSTR_EXAMPLE_CONST), MP_ROM_INT(123) }
+};
