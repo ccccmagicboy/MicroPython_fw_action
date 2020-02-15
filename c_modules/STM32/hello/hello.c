6e1bc893be7193c4a81fe48456b026b00bf562bc
cccc <cuiwei_cv@163.com>
2020-02-15 16:25:25 +0800

    complete a more useful c module template---hello

--- c_modules/STM32/hello/hello.c
+++ c_modules/STM32/hello/hello.c
@@ -4,8 +4,14 @@
 #include "py/builtin.h"
 // 包含标准库
 #include <stdio.h>
+// 包含这个模块用的头文件
+#include "example.h"
+
 // 自定义一个函数
-STATIC mp_obj_t hello_world(mp_obj_t echo_obj) 
+// https://mpy-c-gen.oliverrobson.tech/
+// 以上网址可生成函数接口模板
+
+mp_obj_t hello_world(mp_obj_t echo_obj) 
 {
     const char* echo = mp_obj_str_get_str(echo_obj);
     bool result;
@@ -17,25 +23,24 @@ STATIC mp_obj_t hello_world(mp_obj_t echo_obj)
 
     return mp_obj_new_bool(result);
 }
-// 定义一个对以上函数的引用
-MP_DEFINE_CONST_FUN_OBJ_1(hello_world_obj, hello_world);
-// 这个定义module的所有属性值
-// Table entries are key/value pairs of the attribute name (a string)
-// and the MicroPython object reference.
-// All identifiers and strings are written as MP_QSTR_xxx and will be
-// optimized to word-sized integers by the build system (interned strings).
-STATIC const mp_rom_map_elem_t hello_module_globals_table[] = {
-    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_hello) },
-    { MP_ROM_QSTR(MP_QSTR_hello_world), MP_ROM_PTR(&hello_world_obj) },
-};
-STATIC MP_DEFINE_CONST_DICT(hello_module_globals, hello_module_globals_table);
-// 定义这个module实体对象
-const mp_obj_module_t hello_user_cmodule = {
-    .base = { &mp_type_module },
-    .globals = (mp_obj_dict_t*)&hello_module_globals,
-};
-// 注册这个module，并留一个活口开关
-MP_REGISTER_MODULE(MP_QSTR_hello, hello_user_cmodule, MODULE_HELLO_ENABLED);
+
+mp_obj_t math_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args)
+{
+    mp_arg_check_num(n_args, n_kw, 0, 0, true); //检查参数的个数
+    math_obj_t *self = m_new_obj(math_obj_t);   //分配内存空间
+    self->base.type = &hello_math_obj;          //结构体实体化与类实体关联
+    return MP_OBJ_FROM_PTR(self);               //返回分配的指针
+}
+
+m_obj_t math_add(m_obj_t, self_in, mp_obj_t data)
+{
+    math_obj_t *self = MP_OBJ_TO_PTR(self_in);  //从第一参数里取出类self
+    self->value1 = 100;
+    self->value2 = 200;
+    self->value3 = mp_obj_get_int(data);    //从第二个参数里取出一个整形
+    printf("100 + 200 + %d =\n", self->value3);
+    return mp_obj_new_int(self->value1 + self->value2 + self->value3);//返回计算结果
+}
 
 
 
---
+++ c_modules/STM32/hello/hello.h
@@ -0,0 +1,59 @@
+// 定义hello模块里的常量   
+enum { 
+        CONSTANTS0, 
+        CONSTANTS1,   
+     };
+////////////////////////////////////////////////////////////////////////////////////////
+// 定义hello模块里的函数     
+STATIC mp_obj_t hello_world(mp_obj_t echo_obj);
+// 定义hello模块里函数的引用
+MP_DEFINE_CONST_FUN_OBJ_1(hello_hello_world_obj, hello_world);
+////////////////////////////////////////////////////////////////////////////////////////
+typedef struct _math_obj_t
+{
+    mp_obj_base_t base;/*这个类成员是必须的*/
+    uint16_t value1;//成员变量value1
+    uint16_t value2;//成员变量value2
+    uint16_t value3;//成员变量value3
+}math_obj_t;/*定义math类的结构体*/
+// 定义math类的构造函数
+STATIC mp_obj_t math_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args);
+// 定义math类里的函数     
+m_obj_t math_add(m_obj_t, self_in, mp_obj_t data);
+// 定义math类里函数的引用
+MP_DEFINE_CONST_FUN_OBJ_2(math_add_obj, math_add);
+// 定义math类里的所有属性值对
+STATIC const mp_rom_map_elem_t math_type_locals_table[] = {
+    { MP_ROM_QSTR(MP_QSTR_add), MP_ROM_PTR(&math_add_obj) },/*python暴露名add与函数引用关联*/
+};
+// 定义math类的字典
+STATIC MP_DEFINE_CONST_DICT(math_type_locals, math_type_locals_table);
+// 定义math类的实体
+const mp_obj_type_t hello_math_obj = {
+    .base = { &mp_type_type }, /*这里与module不同*/
+    .name = { MP_QSTR_math }, /*python暴露名在这里定义*/
+    .make_new = math_new,       //构造函数
+    .locals_dict = (mp_obj_dict_t*)&math_type_locals,
+};
+////////////////////////////////////////////////////////////////////////////////////////
+// 定义模块的所有属性值对
+STATIC const mp_rom_map_elem_t hello_module_globals_table[] = {
+    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_hello) }, /*python暴露名hello与python暴露名__name__关联*/
+    { MP_ROM_QSTR(MP_QSTR_hello_world), MP_ROM_PTR(&hello_hello_world_obj) }, /*python暴露名hello_world与函数引用关联*/
+    { MP_ROM_QSTR(MP_QSTR_C0), MP_ROM_INT(CONSTANTS0)}, /*python暴露名C0与常量CONSTANTS0关联*/
+    { MP_ROM_QSTR(MP_QSTR_C1), MP_ROM_INT(CONSTANTS1)}, /*python暴露名C1与常量CONSTANTS1关联*/
+    { MP_ROM_QSTR(MP_QSTR_math), MP_ROM_PTR(&hello_math_obj)}, /*python暴露名math与math类（type）关联*/
+};
+// 定义模块的字典
+STATIC MP_DEFINE_CONST_DICT(hello_module_globals, hello_module_globals_table);
+
+// 定义模块的实体
+const mp_obj_module_t hello_user_cmodule = {
+    .base = { &mp_type_module },
+    .globals = (mp_obj_dict_t*)&hello_module_globals,
+};
+
+// 注册模块实体到暴露名，并关联一个使能常量
+MP_REGISTER_MODULE(MP_QSTR_hello/*python暴露名hello*/, hello_user_cmodule/*模块实体*/, MODULE_HELLO_ENABLED);
+
+
