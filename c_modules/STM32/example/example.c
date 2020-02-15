5dd167fdd12d583c1d5039593be5e877a1d3bfd0
cccc <cuiwei_cv@163.com>
2020-02-15 14:08:32 +0800

    add constant example

--- c_modules/STM32/example/example.c
+++ c_modules/STM32/example/example.c
@@ -2,6 +2,8 @@
 #include "py/obj.h"
 #include "py/runtime.h"
 #include "py/builtin.h"
+// add header file custom
+#include "example.h"
 
 // This is the function which will be called from Python as example.add_ints(a, b).
 STATIC mp_obj_t example_add_ints(mp_obj_t a_obj, mp_obj_t b_obj) {
@@ -21,8 +23,13 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_2(example_add_ints_obj, example_add_ints);
 // All identifiers and strings are written as MP_QSTR_xxx and will be
 // optimized to word-sized integers by the build system (interned strings).
 STATIC const mp_rom_map_elem_t example_module_globals_table[] = {
+    // add a module
     { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_example) },
+    // add a function
     { MP_ROM_QSTR(MP_QSTR_add_ints), MP_ROM_PTR(&example_add_ints_obj) },
+    // add a constant
+    { MP_ROM_QSTR(MP_QSTR_C0), MP_ROM_INT(CONSTANTS0)},
+    { MP_ROM_QSTR(MP_QSTR_C1), MP_ROM_INT(CONSTANTS1)},
 };
 STATIC MP_DEFINE_CONST_DICT(example_module_globals, example_module_globals_table);
 
---
+++ c_modules/STM32/example/example.h
@@ -0,0 +1,9 @@
+
+
+enum { 
+        CONSTANTS0, 
+        CONSTANTS1,   
+     };
+
+
+     
