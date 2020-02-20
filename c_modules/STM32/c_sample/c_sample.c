47eeb94dcb937ecdaddcd77cf0c39b1f23146105
cccc <cuiwei_cv@163.com>
2020-02-20 23:44:29 +0800

    wakey wakey GitHub Actions

--- c_modules/STM32/c_sample/c_sample.c
+++ c_modules/STM32/c_sample/c_sample.c
@@ -1,7 +1,9 @@
 #include "py/nlr.h"
 #include "py/obj.h"
-#include "py/runtime.h"
 #include "py/binary.h"
+#include "py/runtime.h"
+#include "py/mperrno.h"
+#include "py/mphal.h"
 #include "portmodules.h"
 #include <string.h>
 #include <stdio.h>
