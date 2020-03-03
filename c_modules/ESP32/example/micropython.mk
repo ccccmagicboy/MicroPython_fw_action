76b2b742edea6bfe0c619faa0530ea91eaede4c8
cccc <cuiwei_cv@163.com>
2020-03-03 14:24:57 +0800

    add module enable switch to .mk file.

--- c_modules/ESP32/example/micropython.mk
+++ c_modules/ESP32/example/micropython.mk
@@ -5,4 +5,6 @@ SRC_USERMOD += $(EXAMPLE_MOD_DIR)/example.c
 
 # We can add our module folder to include paths if needed
 # This is not actually needed in this example.
-CFLAGS_USERMOD += -I$(EXAMPLE_MOD_DIR)
+CFLAGS_USERMOD += -I$(EXAMPLE_MOD_DIR) -DMODULE_EXAMPLE_ENABLED=1
+
+
--- c_modules/ESP32/hello/micropython.mk
+++ c_modules/ESP32/hello/micropython.mk
@@ -6,4 +6,6 @@ SRC_USERMOD += $(HELLO_MOD_DIR)/hello_math.c
 
 # We can add our module folder to include paths if needed
 # This is not actually needed in this example.
-CFLAGS_USERMOD += -I$(HELLO_MOD_DIR)
+CFLAGS_USERMOD += -I$(HELLO_MOD_DIR) -DMODULE_HELLO_ENABLED=1
+
+
